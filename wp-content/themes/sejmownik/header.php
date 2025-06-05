ac4075d81a822eb3773d9b496258ea3610b1f7ca
Rafał Leśniak <rafal.lesniak10@gmail.com>
2025-06-05 12:04:43 +0200

    Add header

--- wp-content/themes/sejmownik/header.php
+++ wp-content/themes/sejmownik/header.php
@@ -19,31 +19,37 @@
 <body <?php body_class('font-montserrat'); ?>>
 <?php wp_body_open(); ?>
 
-<header>
-    <div>
-        <div>
-            <div>
-                <?php if (has_custom_logo()) : ?>
-                    <div>
-                        <?php
-                        $custom_logo_id = get_theme_mod('custom_logo');
-                        if ($custom_logo_id) {
-                            $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
-                            if ($logo) {
-                                echo '<a href="' . esc_url(home_url('/')) . '" rel="home">';
-                                echo '<img src="' . esc_url($logo[0]) . '" width="250" alt="' . esc_attr(get_bloginfo('name')) . '">';
-                                echo '</a>';
-                            }
-                        }
-                        ?>
+<header class="parlament-gradient shadow-lg">
+    <div class="container mx-auto px-4 py-6">
+        <div class="flex flex-col md:flex-row justify-between items-center">
+            <div class="flex items-center mb-4 md:mb-0">
+                <?php if (has_custom_logo()) : 
+                    $custom_logo_id = get_theme_mod('custom_logo');
+                    $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+                ?>
+                    <div class="bg-white p-2 rounded-full mr-4">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+                            <img src="<?php echo esc_url($logo[0]); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" class="w-12 h-12">
+                        </a>
                     </div>
                 <?php endif; ?>
+                
                 <div>
+                    <h1 class="text-2xl md:text-3xl font-bold text-white"><?php bloginfo('name'); ?></h1>
                     <?php $description = get_bloginfo('description', 'display'); ?>
                     <?php if ($description) : ?>
-                        <p><?php echo $description; ?></p>
+                        <p class="text-white opacity-80"><?php echo $description; ?></p>
+                    <?php else: ?>
+                        <p class="text-white opacity-80">Lista posłów na Sejm RP</p>
                     <?php endif; ?>
                 </div>
+            </div>
+            <div class="relative">
+                <form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
+                    <input type="text" name="s" placeholder="Wyszukaj posła..." 
+                           class="pl-10 pr-4 py-2 rounded-full w-full md:w-64 focus:outline-none focus:ring-2 focus:ring-parlament-gold">
+                    <i class="fas fa-search absolute left-3 top-3 text-gray-400"></i>
+                </form>
             </div>
         </div>
     </div>
