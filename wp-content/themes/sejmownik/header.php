b86f375c53a80b60f90b55c8c7a5b8aa68877ef5
Rafał Leśniak <rafal.lesniak10@gmail.com>
2025-08-05 13:50:31 +0200

    Add Google Analytics gtag.js to header
    Integrated Google Analytics by adding the gtag.js script and configuration to header.php for site tracking.

--- wp-content/themes/sejmownik/header.php
+++ wp-content/themes/sejmownik/header.php
@@ -14,6 +14,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <?php wp_head(); ?>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-V602XWSR9M"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-V602XWSR9M');
+    </script>
 </head>
 
 <body <?php body_class('font-montserrat'); ?>>
